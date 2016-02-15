31f0632a96f3d590f381a2945af8cb39dc7a22fc
max33nau <mtj29@nau.edu>
2016-02-14 21:25:42 -0800

    edit works

--- src/public/controllers/admin-controller.js
+++ src/public/controllers/admin-controller.js
@@ -2,10 +2,25 @@ var app = angular.module('myApp');
 
 app.controller('adminCtrl',['$scope', '$http', function($scope,$http) {
   $scope.error = '';
+  $scope.success = '';
   var token = localStorage.getItem('token');
   $scope.admin = {};
   $scope.playerInfo = {};
+  $scope.players = [];
+  $http({
+    url: '/players',
+    method: 'GET',
+    headers: { 'token': token}
+  })
+  .then(function(response){
+    $scope.players = response.data;
+  })
+  .catch(function(error){
+    $scope.error = 'Something went wrong getting players from the database'
+  })
   $scope.admin.createPlayer = function() {
+    $scope.error = '';
+    $scope.success = '';
     $http({
       url: '/players',
       method: 'POST',
@@ -13,10 +28,12 @@ app.controller('adminCtrl',['$scope', '$http', function($scope,$http) {
       data: $scope.playerInfo
     })
     .then(function(response){
-      console.log(response.data);
+      $scope.success = response.data.name + ' was added to the database.';
     })
     .catch(function(error){
-      console.log(error);
+      $scope.error = 'The right criteria was not met for adding a new player';
     })
   }
+
+
 }]);
